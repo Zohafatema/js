0e5055c84cea35c83ac406b15c31cf14a0f890af
Zoha Fatema Ansari <zohaa0166@gmail.com>
2026-07-02 21:27:38 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+// console.log("STRIKE is Comming");
+// variable
+// let
+// let name = zoha;
+// let age = 25;0
+// console.log("name , age")
+
+// const
+// const account = 1234;
+
+// var b = 30;
+
+// // Data Types
+
+// // Primitive Data Type
+
+// //number,string, boolean, undefined, null, bigint, symbol
+
+
+// // Number
+// let a = 10;
+// let b =2.36;
+// console.log(a,b);
+
+
+// // String
+
+// let c = "Strike is comming";
+// let d = "Anjali";
+// console.log(c,d);
+
+
+// // Boolean 
+// let login = true;
+// let f = false;
+// console.log(login,f);
+
+// //undefine
+// let user;
+// console.log(user);
+
+// // Bigint
+// let bigNumber =123456789565256n;
+
+// // Null
+// // let weather = null;
+// let weather = current_weather("dwarka");
+// // 25
+// // null
+// // undefined
+
+// // Symbol
+// const id1 = Symbol("id");
+// const id2 = Symbol("id");
+// console.log(id2==id1);
+
+// // Non Primitive Data Type
+// // array, object, functions
+
+// // Array
+// let arr =[10, 11, "anjali", true];
+// console.log(arr);
+
+// // Object
+// let user = {
+//     name:"anjali",
+//     account:1234,
+//     age:18,
+//     category:'gen'
+// }
+
+// console.log(user);
+
+// // Function 
+// let s = function add(){
+//     console.log("Hello");
+// }
+// console.log(s); 
